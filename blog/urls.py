ec41f5c84fb1c988d57514c1c42803e5986d724d
shin <shin@naver.com>
2025-06-30 01:59:32 +0900

    10

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.blog_list, name='blog_list'),
     path('write/', views.blog_write, name='blog_write'),
     path('detail/<int:pk>/', views.blog_detail, name='blog_detail'),
-    # 'api/generate-title/' URL 패턴은 더 이상 필요 없으므로 제거
+    # 새로운 AJAX 엔드포인트 추가
+    path('api/get-random-title-components/', views.get_random_title_components, name='get_random_title_components'),
 ]
+
