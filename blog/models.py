15259cfead7cefd20ec110de8f7eac6468340e5d
shin <shin@naver.com>
2025-06-30 00:11:45 +0900

    9

--- blog/models.py
+++ blog/models.py
@@ -1,42 +1,44 @@
-from django.db import models
-from django.utils import timezone
-import random
+from django.contrib import admin
+from .models import Blog, ContentTitle, ContentNumberCharacter, ContentTalkstyle, ContentAspect
 
-class Blog(models.Model):
-    title = models.CharField(max_length=100, blank=True)
-    content = models.TextField()
-    client_type = models.CharField(max_length=20)
-    image_url = models.URLField(max_length=50)
-    place_name = models.CharField(max_length=20)
-    content_title = models.CharField(max_length=20)
-    content_number_character = models.CharField(max_length=20)
-    content_talkstyle = models.CharField(max_length=20)
-    content_aspect = models.CharField(max_length=20)
-    written_date = models.DateField(default=timezone.now)
-    
-    def save(self, *args, **kwargs):
-        # title을 4개 구성요소의 랜덤 조합으로 생성11
-        if not self.title:
-            components = [
-                self.content_title,
-                self.content_number_character,
-                self.content_talkstyle,
-                self.content_aspect
-            ]
-            # 빈 값이 아닌 것들만 필터링
-            valid_components = [comp for comp in components if comp.strip()]
-            
-            if valid_components:
-                # 랜덤하게 섞어서 조합
-                random.shuffle(valid_components)
-                self.title = ' '.join(valid_components)
-            else:
-                self.title = "제목 없음"
-                
-        super().save(*args, **kwargs)
-    
-    def __str__(self):
-        return self.title
-    
-    class Meta:
-        ordering = ['-written_date']
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'place_name', 'client_type', 'written_date']
+    list_filter = ['client_type', 'written_date', 'place_name']
+    search_fields = ['title', 'content', 'place_name']
+    readonly_fields = ['title']  # title은 자동 생성되므로 읽기 전용
+
+    fieldsets = (
+        ('기본 정보', {
+            'fields': ('title', 'content', 'client_type', 'image_url', 'place_name')
+        }),
+        # 제목 구성 요소 필드는 이제 폼에서 사라지므로 admin 필드셋에서도 제거
+        # ('제목 구성 요소', {
+        #     'fields': ('content_title', 'content_number_character', 'content_talkstyle', 'content_aspect')
+        # }),
+        ('기타', {
+            'fields': ('written_date',)
+        }),
+    )
+
+# --- 새로운 모델 Admin 등록 시작 ---
+
+@admin.register(ContentTitle)
+class ContentTitleAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(ContentNumberCharacter)
+class ContentNumberCharacterAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(ContentTalkstyle)
+class ContentTalkstyleAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(ContentAspect)
+class ContentAspectAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
