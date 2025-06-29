0f7e3471e5da63be6a18bcb19ad7ef7ca533bc92
shin <shin@naver.com>
2025-06-30 04:34:57 +0900

    10

--- blog/views.py
+++ blog/views.py
@@ -14,12 +14,12 @@ from datetime import datetime    # 날짜 처리를 위해 추가
 def blog_list_completed(request):
     """블로그 목록 페이지 - blog_write가 True인 작성 완료된 글만 표시"""
     blogs_completed = Blog.objects.filter(blog_write=True).select_related('client').order_by('-written_date')
-    return render(request, 'blog/list_completed.html', {'blogs': blogs_completed, 'list_title': '작성 완료된 블로그 글'})
+    return render(request, 'blog/list_completed.html', {'blogs': blogs_completed, 'list_title': '트래픽 용도 글'})
 
 def blog_list_pending(request):
     """블로그 목록 페이지 - blog_write가 False인 작성 대기 중인 글만 표시"""
     blogs_pending = Blog.objects.filter(blog_write=False).select_related('client').order_by('-written_date')
-    return render(request, 'blog/list_pending.html', {'blogs': blogs_pending, 'list_title': '트래픽용 제목'})
+    return render(request, 'blog/list_pending.html', {'blogs': blogs_pending, 'list_title': '포스팅용도 글 '})
 
 
 def blog_write(request):
