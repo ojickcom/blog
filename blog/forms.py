b9f3754a2197a50e5987cbc7d834f91fae32652b
shin <shin@naver.com>
2025-07-14 23:30:14 +0900

    13

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,46 @@
 from django import forms
 from .models import Blog, Client, ShoppingKeyword
 
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ['client', 'b_title', 'title', 'content']
+        widgets = {
+            'client': forms.Select(attrs={
+                'class': 'form-select',
+            }),
+            'b_title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'B_제목을 입력하세요 (선택 사항)'
+            }),
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': '블로그 제목을 입력하세요 (비워두면 자동 생성)'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 10,
+                'placeholder': '10자 이상의 내용을 입력하세요'
+            }),
+        }
+        labels = {
+            'client': '클라이언트',
+            'b_title': 'B_제목',
+            'title': '제목',
+            'content': '내용',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['client'].queryset = Client.objects.all().order_by('name')
+        self.fields['client'].empty_label = "--- 클라이언트를 선택하세요 ---"
+
+    def clean_content(self):
+        content = self.cleaned_data['content']
+        if len(content) < 50:
+            raise forms.ValidationError('내용은 50자 이상이어야 합니다.')
+        return content
+
 class ShoppingKeywordForm(forms.ModelForm):
     # 키워드 그룹 선택 필드 정의
     KEYWORD_GROUP_CHOICES = [
